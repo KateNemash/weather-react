8040f322f1e39d482e7c09344378fc6d70645430
KateNemash <katee0907@gmail.com>
2023-03-27 23:15:13 +0200

    Deleted unit conversion and added forecast update

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,4 @@
 import React from "react";
-import UnitConvertion from "./UnitConvertion";
 
 import "./Weather.css";
 
@@ -7,7 +6,13 @@ export default function Weather(props) {
   return (
     <div className="Weather">
       <div className="row center">
-        <UnitConvertion celsius={props.data.temperature} />
+        <div className="col temperature">
+          <span className="changing-temp">
+            {Math.round(props.data.temperature)}
+          </span>
+          <span className="temperature-units">°C</span>
+        </div>
+
         <div className="col">
           <img
             src={props.data.icon}
