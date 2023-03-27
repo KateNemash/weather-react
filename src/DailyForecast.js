e46a79f9665675966b80af46b0f992d3c64ddf13
KateNemash <katee0907@gmail.com>
2023-03-27 22:54:45 +0200

    Duplicated the days in the forecast

--- src/DailyForecast.js
+++ src/DailyForecast.js
@@ -15,8 +15,18 @@ export default function DailyForecast(props) {
   if (loaded) {
     return (
       <div className="DailyForecast">
-        <div className="col">
-          <ForecastDay data={forecast[0]} />
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <ForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
         </div>
       </div>
     );
